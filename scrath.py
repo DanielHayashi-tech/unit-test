00b9c80c7bf314432ea63f593b3a706112b35af6
dhayashi <dhayashi@intuitivemachines.com>
2024-04-05 18:34:24 +0000

    removed scratch file

--- scrath.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-day = int(input("Enter a day: "))
-month = int(input("Enter a month: "))
-year = int(input("Enter a year: "))
-
-lemme_see = datetime.datetime(year, month, day)
-print(lemme_see)
